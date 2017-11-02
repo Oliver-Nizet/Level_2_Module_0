2b37c540e186e4d6175981224415217f42a4c3a7
ShadowWolf <league@WTS15.attlocal.net>
2017-11-01 19:59:58 -0700

    skrrra

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,14 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +17,31 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame;
+	JPanel panel;
+	JButton add;
+	JButton view;
+	public static void main(String[] args) {
+		ArrayList<String> names = new ArrayList<String>();
+		GuestBook gb = new GuestBook();
+	}
+	GuestBook(){
+		frame = new JFrame();
+		panel = new JPanel();
+		add = new JButton();
+		view = new JButton();
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		add.setText("Add Name");
+		view.setText("View Names");
+		add.addActionListener(this);
+		view.addActionListener(this);
+		frame.setVisible(true);
+		frame.pack();
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,42 @@
 package intro_to_array_lists;
+import java.util.ArrayList;
 
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-		
+		ArrayList<String> colors = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		colors.add("red");
+		colors.add("orange");
+		colors.add("yellow");
+		colors.add("green");
+		colors.add("blue");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i < colors.size(); i++){
+			String s = colors.get(i);
+			System.out.println("Set 1: " + s);
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s : colors){
+			System.out.println("Set 2: " + s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i = 0; i < colors.size(); i+=2){
+			String s = colors.get(i);
+			System.out.println("Set 3: " + s);
+		}
 		//6. Print all the Strings in reverse order.
-		
+		for(int i = colors.size()-1; i > -1; i--){
+			String s = colors.get(i);
+			System.out.println("Set 4: " + s);
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for(int i = 0; i < colors.size(); i++){
+			String s = colors.get(i);
+			if (s.contains("e")) {
+				System.out.println("Set 5: " + s);
+			}
+		}
 	}
 }
